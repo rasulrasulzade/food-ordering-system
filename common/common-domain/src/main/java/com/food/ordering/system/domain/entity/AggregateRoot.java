8fa3e70be91f808b37551b700d8f678b37cb5c07
rasul.rasulzade <rasul.rasulzade@cybernet.az>
2023-02-13 12:21:50 +0400

    Implement Order creation

--- common/common-domain/src/main/java/com/food/ordering/system/domain/entity/AggregateRoot.java
+++
@@ -1,6 +0,0 @@
-package com.food.ordering.system.domain.entity;
-
-import com.food.ordering.system.domain.entity.BaseEntity;
-
-public class AggregateRoot<ID> extends BaseEntity<ID> {
-}
--- common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/PaymentStatus.java
+++
@@ -1,5 +0,0 @@
-package com.food.ordering.system.domain.valueobject;
-
-public enum PaymentStatus {
-    CREATED, CANCELLED, FAILED
-}
